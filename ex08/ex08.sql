3d1d4fb674896ddaf10edda2ffca47adcf1f087b
mchindri <chindris97@yahoo.es>
2016-04-12 17:08:48 +0000

    Da

--- ex08/ex08.sql
+++ ex08/ex08.sql
@@ -1,1 +1,1 @@
-SELECT nom, prenom, date_naissance 'date de naissance' FROM fiche_personne WHERE YEAR(date_naissance) = 1989 ORDER BY nom;
+SELECT nom, prenom, DATE(date_naissance) 'date de naissance' FROM fiche_personne WHERE YEAR(date_naissance) = 1989 ORDER BY nom;
